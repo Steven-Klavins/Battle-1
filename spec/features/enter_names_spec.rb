8c01556685ee924b15434a51463c31b8237c3249
Steven-Klavins <stevenklavins94@gmail.com>
2020-04-02 15:31:22 +0100

    added get to play erb

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 feature "Entering players" do
   scenario "player enters name in form and sees on screen" do
-    visit ('/names')
+    visit ('/')
     fill_in :player_1_name, with: "Bob"
     fill_in :player_2_name, with: "Anna"
     click_button 'Submit'
--- spec/features/testing_infrastructure_working_spec.rb
+++
@@ -1,9 +0,0 @@
-require "spec_helper"
-
-feature "testing infrastructure is working" do
-  scenario "Infrastructure works" do
-    visit('/')
-    expect(page).to have_content 'Testing infrastructure working!'
-  end
-end
-
