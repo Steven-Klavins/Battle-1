a80bd548bb345ba0dfb175dccabe9abeda4ca958
Edward Phillips <edd0ddi0@gmail.com>
2020-04-03 15:49:14 +0100

    refactor - completed removal of references to other global variables

--- app.rb
+++ app.rb
@@ -10,21 +10,13 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    pp session
     $game = Game.new(Player.new(params[:player_1_name]),Player.new(params[:player_2_name]))
-
-    @player_1_name = $game.player_one.name
-    @player_2_name = $game.player_two.name
-
-    session[:player_1_name] = @player_1_name
-    session[:player_2_name] = @player_2_name
+  
     redirect '/play'
   end
   
   get '/play' do
-    @player_1_name = $game.player_one.name
-    @player_2_name = $game.player_two.name
-    @player_2_hp = $game.player_two.hp
+    @game = $game
 
     @message = session[:message]
 
@@ -32,8 +24,7 @@ class Battle < Sinatra::Base
   end
 
   post '/attack' do
-    @player_1_name = $game.player_one.name
-    @player_2_name = $game.player_two.name
+    @game = $game
 
     @message = params[:attack]
     session[:message] = @message
