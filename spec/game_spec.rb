7428022f2d160f8a12822a026c21977c180e6caa
Steven-Klavins <stevenklavins94@gmail.com>
2020-04-03 14:53:27 +0100

    added another feature test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,8 +4,8 @@ require 'game'
 describe Game  do
     let (:player) { double :player }
     it 'Causes player to take damage' do
+        expect(player).to receive(:take_damage)
         subject.attack(player)
-        expect(player).to receive :take_damage 
     end 
 end
 
